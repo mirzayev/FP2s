cb8c3fa110ff3f5300a0fa299b03f7962727e49d
Farid Mirzayev <mirzayevfarid@gmail.com>
2019-03-06 17:50:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.getElementById("btn").addEventListener("click", () => {
   setTimeout(() => {
       new Fingerprint2().get(function(result, components)
     {
+        
+        document.querySelector("#value").textContent = result;
         console.log("fph "+result)
         // console.log(components)
 
